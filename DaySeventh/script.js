1768a8c5b3001a61a54161d2208e7cf365fe69c5
Samzy07 <surusamson7@gmail.com>
2024-11-24 16:15:48 +0100

    Day 17 DOM Manipulation

---
+++ DaySeventh/script.js
@@ -0,0 +1,6 @@
+let textEl = document.getElementById('text')
+let buttonEl = document.querySelector('.button')
+
+buttonEl.addEventListener("click", ()=>{
+    textEl.innerHTML = `Logged In Succefully!`
+})
